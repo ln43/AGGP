998e98838b1f0b5cd32a90814c6b3a874b5ca168
VASSAL Hélène <helene.vassal@insa-lyon.fr>
2017-02-15 11:41:53 +0100

    Ajout stat P(k) et C(k)

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 from Graphes import Graphes
 import networkx as nx
 import matplotlib.pyplot as plt
-from scipy import stats
 import numpy as np
 
 #//////////////////////// MAIN //////////////////////////////////////
@@ -28,6 +27,8 @@ for i in range(1,len(PK)):
 X=[(PK[i]-PKtheo[i])**2 for i in range(1,len(PK))]
 Xsum=sum(X)
 print Xsum
+
+print Gr.stat_Pk(gamma)
 
 #Gtry=nx.gnm_random_graph(10,15)
 
