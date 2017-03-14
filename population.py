507ddc336f9d1cad5e93fcff22ca121db4c7eb55
ln43 <helene.vassal@insa-lyon.fr>
2017-03-14 16:47:28 +0100

    correction croisement

--- population.py
+++ population.py
@@ -55,38 +55,43 @@ class population :
             while (popSelectionnee[i]==g):
               i=np.random.randint(0,len(popSelectionnee))
             g2=popSelectionnee[i]
+            
+            #Scinder les noeuds de g en deux groupes
             noeuds1=np.random.choice(g.G.nodes(),int(g.n/2),replace=False)
             noeudsopp1=filter(lambda x: x not in noeuds1, g.G.nodes())
-            m1=g.m-nx.number_of_edges(g.G.subgraph(noeuds1))
+            m1=g.m-nx.number_of_edges(g.G.subgraph(noeuds1))-nx.number_of_edges(g.G.subgraph(noeudsopp1))
             
+            #Renommer noeud de g2
             mapping={}
             for i in range(g2.n):
               mapping[i]=i+g.n
             g2.G=nx.relabel_nodes(g2.G,mapping)
             
+            #Scinder les noeuds de g2 en deux groupes
             noeuds2=np.random.choice(g2.G.nodes(),int(g2.n/2),replace=False)
             noeudsopp2=filter(lambda x: x not in noeuds2, g2.G.nodes())
-            m2=g2.m-nx.number_of_edges(g2.G.subgraph(noeuds2))
+            m2=g2.m-nx.number_of_edges(g2.G.subgraph(noeuds2))-nx.number_of_edges(g.G.subgraph(noeudsopp2))
             
+            #Creation de deux nouveaux graphes
             newg1=nx.compose(g.G.subgraph(noeuds1),g2.G.subgraph(noeudsopp2))
             newg2=nx.compose(g2.G.subgraph(noeuds2),g.G.subgraph(noeudsopp1))
-            
+
             i=0
-            while i<m1 and nx.number_of_edges(newg1)<(nx.number_of_nodes(newg1)*(nx.number_of_nodes(newg1)-1)/2):
+            while i<min(m1,len(noeuds1)*len(noeudsopp2)):
               e=(np.random.choice(noeuds1,1),np.random.choice(noeudsopp2,1))
               if e not in newg1.edges():
                 newg1.add_edge(int(e[0]),int(e[1]))
-                i=i+1    
+                i=i+1
             g.G=nx.convert_node_labels_to_integers(newg1)
             g.G=g.connected_Graph(g.G)
             g.n,g.m=nx.number_of_nodes(g.G),nx.number_of_edges(g.G)
             
             i=0
-            while i<m2 and nx.number_of_edges(newg2)<(nx.number_of_nodes(newg2)*(nx.number_of_nodes(newg2)-1)/2):
+            while i<min(m2,len(noeuds2)*len(noeudsopp1)):
               e=(np.random.choice(noeuds2,1),np.random.choice(noeudsopp1,1))
               if e not in newg2.edges():
                 newg2.add_edge(int(e[0]),int(e[1]))
-                i=i+1     
+                i=i+1   
             g2.G=nx.convert_node_labels_to_integers(newg2)
             g2.G=g2.connected_Graph(g2.G)
             g2.n,g2.m=nx.number_of_nodes(g2.G),nx.number_of_edges(g2.G)
